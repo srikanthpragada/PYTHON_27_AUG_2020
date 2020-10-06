3d5810a5bceca1c129cd825e579eb711f38060aa
srikanthpragada <srikanthpragada@gmail.com>
2020-10-06 19:52:36 +0530

    06-OCT

--- website/basics/interest_views.py
+++ website/basics/interest_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import InterestForm
 
 
 def interest(request):
@@ -11,3 +12,46 @@ def interest(request):
                       {'amount': amount, 'rate': rate, 'interest': interest})
     else:
         return render(request, 'interest.html')
+
+
+def interest_post(request):
+    if request.method == 'POST':
+        amount = float(request.POST['amount'])
+        rate = float(request.POST['rate'])
+        interest = amount * rate / 100
+        return render(request, 'interest_post.html',
+                      {'amount': amount, 'rate': rate, 'interest': interest})
+    else:  # GET
+        return render(request, 'interest_post.html')
+
+
+def interest_form(request):
+    if request.method == "POST":
+        form = InterestForm(request.POST)  # bind POST data to form
+        if form.is_valid():
+            amount = float(form.cleaned_data['amount'])
+            rate = float(form.cleaned_data['rate'])
+            interest = amount * rate / 100
+            return render(request, 'interest_form.html',
+                          {'form': form, 'interest': interest})
+        else:
+            return render(request, 'interest_form.html', {'form': form})
+    else:  # GET request
+        form = InterestForm()   # Empty form
+        return render(request, 'interest_form.html', {'form': form})
+
+
+def interest_form_custom(request):
+    if request.method == "POST":
+        form = InterestForm(request.POST)  # bind POST data to form
+        if form.is_valid():
+            amount = float(form.cleaned_data['amount'])
+            rate = float(form.cleaned_data['rate'])
+            interest = amount * rate / 100
+            return render(request, 'interest_form_custom.html',
+                          {'form': form, 'interest': interest})
+        else:
+            return render(request, 'interest_form_custom.html', {'form': form})
+    else:  # GET request
+        form = InterestForm()
+        return render(request, 'interest_form_custom.html', {'form': form})
--- website/basics/urls.py
+++ website/basics/urls.py
@@ -1,11 +1,18 @@
-
 from django.urls import path
-from . import views, interest_views
+from . import views, interest_views, author_views
 
 urlpatterns = [
-    path('welcome', views.welcome),
-    path('topics', views.topics),
-    path('list_topics', views.send_topics),
-    path('wish', views.wish),
-    path("interest", interest_views.interest),
+    path('welcome/', views.welcome),
+    path('topics/', views.topics),
+    path('list_topics/', views.send_topics),
+    path('wish/', views.wish),
+    path("interest/", interest_views.interest),
+    path("interest_post/", interest_views.interest_post),
+    # Author URLS
+    path("authors/", author_views.list_authors),
+    path("add_author/", author_views.add_author),
+    path("add_author_form/", author_views.add_author_form),
+    # Form URLs
+    path("interest_form/", interest_views.interest_form),    # using Django form
+    path("interest_form_custom/", interest_views.interest_form_custom)
 ]
